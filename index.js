9b6147189a0ae96705530d571f5159e230fb17f5
Renan Muniz <renan@renanmuniz.com>
2016-09-20 10:57:03 -0300

    Update index.js, clean index.js

--- index.js
+++ index.js
@@ -16,22 +16,6 @@ app.use(express.static(__dirname + '/build'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', function(request, response) {
-	response.send("hi!");
-});
-
-app.get('/manifest.json', function(req, res) {
-  fs.readFile(MANIFEST_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    res.set('Content-Type', 'application/json');
-    res.send(data);
-  });
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
